e196bd1c0f1dc89dacc6a8e34796ef1e243ea05b
Daniel1209 <danielgarciamendoza.204@gmail.com>
2022-11-28 22:32:50 -0600

    First commit

--- AEIOU/App.js
+++ AEIOU/App.js
@@ -1,101 +1,261 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
+import {Keyframe, Easing, useSharedValue,useAnimatedStyle, withRepeat, withTiming} from 'react-native-reanimated';
+import Animated from 'react-native-reanimated';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [position, setPosition] = useState({x: 50, y: 500});
+  const [positions, setPositions] = useState([
+    {x: 100, y: 500},
+    {x: 200, y: 500},
+    {x: 300, y: 500},
+    {x: 400, y: 500},
+    {x: 500, y: 500},
+  ]);
+  const opacity = useSharedValue(0);
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+  // Set the opacity value to animate between 0 and 1
+  opacity.value = withRepeat(
+    withTiming(1, { duration: 1000, easing: Easing.ease }),
+    -1,
+    true
   );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const style = useAnimatedStyle(() => ({ opacity: opacity.value, height: 100, width:100, backgroundColor:'red' }), []);
+  // const enteringAnimation = new Keyframe({
+  //   0: {
+  //     originX: 0,
+  //     originY: 200,
+  //     backgroundColor: 'red',
+  //     // transform: [{translate: ' 200px 0px'}],
+  //   },
+  //   25: {
+  //     originX: 200,
+  //     originY: 200,
+  //     backgroundColor: 'green',
+  //     borderRadius: '50%',
+  //     // transform: [{translate: '200px 200px'}],
+  //   },
+  //   50: {
+  //     originX: 200,
+  //     originY: -200,
+  //     backgroundColor: 'yellow',
+  //     // rotate: '45deg',
+  //     // scale: 2,
+  //     // transform: [{rotate: '45deg', scale: 2}],
+  //   },
+  //   75: {
+  //     originX: 0,
+  //     originY: -200,
+  //     backgroundColor: 'white',
+  //     // scale: 1,
+  //     // transform: [{scale: 1}],
+  //   },
+  //   100: {
+  //     originX: 0,
+  //     originY: 200,
+  //     // rotate: '-45deg,',
+  //     // transform: [{rotate: '-45deg,'}],
+  //     easing: Easing.quad,
+  //   },
+  // }).duration(2000);
+  const a = Math.floor(Math.random() * (700 - 500 + 1) + 500);
+  const b = Math.floor(Math.random() * (700 - 500 + 1) + 500);
+  const c = Math.floor(Math.random() * (500 - 0 + 1) + 0);
+  const d = Math.floor(Math.random() * (500 - 0 + 1) + 0);
+  console.log('Number ', a, b,c,d);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
+  const enteringAnimation = new Keyframe({
+    0: {
+      originX: a,
+      originY: b,
+      // backgroundColor: 'red',
+      // transform: [{rotate: '45deg'}],
+    },
+    25: {
+      originX: c,
+      originY: d,
+      // transform: [{rotate: '45deg'}],
+    },
+    50: {
+      originX: a,
+      originY: c,
+      // transform: [{scaleX: 2}],
+    },
+    70: {
+      originX: c,
+      originY: d,
+      // transform: [{rotate: '-90deg'}],
+    },
+    100: {
+      originX: d,
+      originY: d,
+      // transform: [{rotate: '0deg'}],
+      easing: Easing.quad,
+    },
+  }).duration(4000);
+
+  const exitingAnimation = new Keyframe({
+    0: {
+      opacity: 1,
+      transform: [{skewX: '0deg'}],
+    },
+    30: {
+      opacity: 0.5,
+      transform: [{skewX: '40deg'}],
+      easing: Easing.exp,
+    },
+    100: {
+      opacity: 0,
+      transform: [{skewX: '-10deg'}],
+    },
+  }).duration(2000);
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+    // All screen
+    <SafeAreaView style={{height: '100%', width: '100%'}}>
+      <View style={styles.viewMain}>
+        {/* First Panel Invicible */}
         <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          style={styles.panelOne}
+          onTouchStart={e => {
+            console.log('Miraaaa View 1 -> ');
+            const positionNow = {
+              x: e.nativeEvent.locationX,
+              y: e.nativeEvent.locationY,
+            };
+            setPosition(positionNow);
+            console.log(e.nativeEvent.locationX, ' ', e.nativeEvent.locationY);
+          }}
+        />
+        {/* Second Panel blue */}
+        <View style={styles.panel}>
+          <Animated.View
+            entering={enteringAnimation}
+            style={style}
+          />
+          <Text
+            style={{
+              ...styles.contenLetter,
+              left: positions[0].x - 35,
+              top: positions[0].y - 30,
+              '@keyframes myEffect': {
+                '0%': {},
+                '25%': {},
+                '50%': {},
+                '75%': {},
+                '100%': {},
+              },
+            }}>
+            A
+          </Text>
+          <Text
+            style={{
+              ...styles.contenLetter,
+              left: positions[1].x - 35,
+              top: positions[1].y - 30,
+            }}>
+            E
+          </Text>
+          <Text
+            style={{
+              ...styles.contenLetter,
+              left: positions[2].x - 35,
+              top: positions[2].y - 30,
+            }}>
+            I
+          </Text>
+          <Text
+            style={{
+              ...styles.contenLetter,
+              left: positions[3].x - 35,
+              top: positions[3].y - 30,
+            }}>
+            O
+          </Text>
+          <Text
+            style={{
+              ...styles.contenLetter,
+              left: positions[4].x - 35,
+              top: positions[4].y - 30,
+            }}>
+            U
+          </Text>
         </View>
-      </ScrollView>
+      </View>
+      <View
+        style={{
+          position: 'absolute',
+          minHeight: 200,
+          minWidth: 200,
+          backgroundColor: 'blue',
+          transform: [{translateX: -121}],
+        }}>
+        {/* <Button
+          title="Press me"
+          onPress={() => {
+            console.log('Holaaa');
+            const a = positions;
+            for (let i = 0; i < 5; i += 1) {
+              a[i].x = position.x;
+            }
+            setPositions(a);
+          }}
+        /> */}
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  viewMain: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    zIndex: 0,
+    backgroundColor: 'orange',
+  },
+  panelOne: {
+    minHeight: 200,
+    minWidth: 200,
+    height: '80%',
+    width: '50%',
+    zIndex: 40,
+    position: 'absolute',
+    opacity: 0,
+    backgroundColor: 'yellow',
+  },
+  panel: {
+    minHeight: 200,
+    minWidth: 200,
+    height: '80%',
+    width: '50%',
+    flexWrap: 'wrap',
+    zIndex: 0,
+    position: 'absolute',
+    backgroundColor: 'blue',
+  },
+  contenLetter: {
+    width: 70,
+    height: 70,
+    textAlign: 'center',
+    margin: 1,
+    zIndex: 10,
+    position: 'absolute',
+    fontSize: 40,
+    borderRadius: 100,
+    backgroundColor: 'red',
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
---
+++ AEIOU/Globos.js
@@ -0,0 +1,326 @@
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  withRepeat,
+  withSequence,
+  withTiming,
+  withSpring,
+} from 'react-native-reanimated';
+import globoImg from './assets/globo.png';
+import abejaImg from './assets/abeja.png';
+import elefanteImg from './assets/elefante.jpeg';
+import iglesiaImg from './assets/iglesia.jpg';
+import osoImg from './assets/oso.png';
+import unicorniImg from './assets/unicornio2.png';
+import audioA from './assets/vocalA.mp3';
+import audioE from './assets/vocalE.mp3';
+import audioI from './assets/vocalI.mp3';
+import audioO from './assets/vocalO.mp3';
+import audioU from './assets/vocalU.mp3';
+import errorAudio from './assets/errorsonido.mp3';
+
+import Sound from 'react-native-sound';
+
+const Globos = () => {
+  const [letters, setLetters] = useState([
+    {
+      name: 'A',
+      img: abejaImg,
+      audio: audioA,
+      labelImg: 'A-BE-JA',
+      x: Math.floor(Math.random() * (700 - 0 + 1) + 0),
+      y: Math.floor(Math.random() * (300 - 0 + 1) + 0),
+    },
+    {
+      name: 'E',
+      img: elefanteImg,
+      audio: audioE,
+      labelImg: 'E-LE-FAN-TE',
+      x: Math.floor(Math.random() * (700 - 0 + 1) + 0),
+      y: Math.floor(Math.random() * (300 - 0 + 1) + 0),
+    },
+    {
+      name: 'I',
+      img: iglesiaImg,
+      audio: audioI,
+      labelImg: 'I-GLE-SI-A',
+      x: Math.floor(Math.random() * (700 - 0 + 1) + 0),
+      y: Math.floor(Math.random() * (300 - 0 + 1) + 0),
+    },
+    {
+      name: 'O',
+      img: osoImg,
+      audio: audioO,
+      labelImg: 'O-SO',
+      x: Math.floor(Math.random() * (700 - 0 + 1) + 0),
+      y: Math.floor(Math.random() * (300 - 0 + 1) + 0),
+    },
+    {
+      name: 'U',
+      img: unicorniImg,
+      audio: audioU,
+      labelImg: 'U-NI-COR-NI-O',
+      x: Math.floor(Math.random() * (700 - 0 + 1) + 0),
+      y: Math.floor(Math.random() * (300 - 0 + 1) + 0),
+    },
+  ]);
+  const [position, setPosition] = useState(0);
+  const [maxWidth, setMaxWidth] = useState(
+    Dimensions.get('window').width - 500,
+  );
+  const [maxHeigth, setMaxHeigth] = useState(
+    Dimensions.get('window').height - 450,
+  );
+
+  const playSound = sss => {
+    const whoosh = new Sound(sss, error => {
+      if (error) {
+        console.log('failed to load the sound', error);
+        return;
+      }
+      // console.log('No hubo fallos');
+      // console.log('Miraaa ', whoosh.getDuration(), whoosh.getVolume());
+      whoosh.play(() => whoosh.release());
+    });
+  };
+
+  const checkLetter = lett => {
+    console.log('Pressed ', lett);
+    const arrayNow = [];
+
+    if (letters[position].name === lett) {
+      console.log('Siiiii');
+      playSound(letters[position].audio);
+      setPosition((position + 1) % 5);
+    } else {
+      console.log('Noooo');
+      playSound(errorAudio);
+    }
+
+    for (let i = 0; i < 5; i += 1) {
+      arrayNow.push({
+        ...letters[i],
+        x: Math.floor(Math.random() * (maxWidth - 1 + 1)) + 1,
+        y: Math.floor(Math.random() * (maxHeigth - 1 + 1)) + 1,
+      });
+    }
+    setLetters(arrayNow);
+  };
+
+  const glowAnimation = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: withRepeat(
+          withSequence(
+            withTiming(1.2, {duration: 1500}),
+            withTiming(1.6, {duration: 1500}),
+            withTiming(1.8, {duration: 1500}),
+          ),
+          -1,
+          true,
+        ),
+      },
+    ],
+  }));
+
+  const animatedMove = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateY: withRepeat(
+          withSequence(
+            withTiming(0.1, {duration: 2500}),
+            withTiming(120.1, {duration: 2500}),
+            withTiming(0.1, {duration: 2500}),
+            //   withTiming(Math.random() * 100, {duration: 2500}),
+          ),
+        ),
+      },
+    ],
+  }));
+
+  return (
+    <View style={styles.viewMain}>
+      {/* View left */}
+      <View style={styles.viewLeft}>
+        <View style={styles.conteinerLetter}>
+          <Text style={{fontSize: 40, textAlign: 'center'}}>Explota</Text>
+          <Animated.View style={[styles.glowContainer, glowAnimation]}>
+            <Text
+              onPress={() => playSound(letters[position].audio)}
+              style={{fontSize: 100, textAlign: 'center'}}>
+              {letters[position].name}
+            </Text>
+          </Animated.View>
+        </View>
+        {/* Image Animation */}
+        {/* <Animated.View style={[styles.contenerImage, animatedMove]}> */}
+        <View style={styles.contenerImage}>
+          <Image style={styles.imgCover} source={letters[position].img} />
+          <Text
+            onPress={() => playSound(letters[position].audio)}
+            style={{fontSize: 35, textAlign: 'center', color: 'white'}}>
+            {letters[position].labelImg}
+          </Text>
+        </View>
+        {/* </Animated.View> */}
+      </View>
+
+      {/* View Rigth */}
+      <View
+        style={{backgroundColor: '#d2ecff', width: '78%', borderRadius: 30}}>
+        {/* A */}
+        <View
+          style={{
+            height: 100,
+            width: 100,
+            alignItems: 'center',
+            flexDirection: 'column',
+            left: letters[0].x,
+            top: letters[0].y,
+          }}>
+          <Image style={styles.imgGlobo} source={globoImg} />
+          <Text
+            style={styles.styleLeter}
+            mode="contained"
+            onTouchStart={() => checkLetter('A')}>
+            A
+          </Text>
+        </View>
+        {/* E */}
+        <View
+          style={{
+            height: 100,
+            width: 100,
+            alignItems: 'center',
+            flexDirection: 'column',
+            left: letters[1].x,
+            top: letters[1].y,
+          }}>
+          <Image style={styles.imgGlobo} source={globoImg} />
+          <Text
+            style={styles.styleLeter}
+            mode="contained"
+            onTouchStart={() => checkLetter('E')}>
+            E
+          </Text>
+        </View>
+        {/* I */}
+        <View
+          style={{
+            height: 100,
+            width: 100,
+            alignItems: 'center',
+            flexDirection: 'column',
+            left: letters[2].x,
+            top: letters[2].y,
+          }}>
+          <Image style={styles.imgGlobo} source={globoImg} />
+          <Text
+            style={styles.styleLeter}
+            mode="contained"
+            onTouchStart={() => checkLetter('I')}>
+            I
+          </Text>
+        </View>
+        {/* O */}
+        <View
+          style={{
+            height: 100,
+            width: 100,
+            alignItems: 'center',
+            flexDirection: 'column',
+            left: letters[3].x,
+            top: letters[3].y,
+          }}>
+          <Image style={styles.imgGlobo} source={globoImg} />
+          <Text
+            style={styles.styleLeter}
+            mode="contained"
+            onTouchStart={() => checkLetter('O')}>
+            O
+          </Text>
+        </View>
+        {/* U */}
+        <View
+          style={{
+            height: 100,
+            width: 100,
+            alignItems: 'center',
+            flexDirection: 'column',
+            left: letters[4].x,
+            top: letters[4].y,
+          }}>
+          <Image style={styles.imgGlobo} source={globoImg} />
+          <Text
+            style={styles.styleLeter}
+            mode="contained"
+            onTouchStart={() => checkLetter('U')}>
+            U
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewMain: {
+    height: '100%',
+    width: '100%',
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: 'white',
+  },
+  imgGlobo: {
+    height: 91,
+    width: 51.5,
+  },
+  styleLeter: {
+    top: -95,
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+    padding: 10,
+  },
+  imgCover: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  viewLeft: {
+    backgroundColor: '#9ca2ef',
+    height: '100%',
+    width: '20%',
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+  },
+  glowContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 20,
+    bottom: 0,
+    left: 0,
+    right: 4,
+  },
+  conteinerLetter: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginTop: 10,
+    height: 250,
+    width: '95%',
+  },
+  contenerImage: {
+    width: '95%',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    marginTop: 10,
+    height: 300,
+  },
+});
+
+export default Globos;
